a3d8189f8d669a3679c370346124020caf3ee119
Gopi vishwakarma <g2468vi@gmail.com>
2023-07-27 20:48:53 +0530

    initial

---
+++ artificial/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3 className='text-2xl bg:red'>my name is gopi vishwakarma</h3>
+    </div>
+  );
+}
+
+export default App;
